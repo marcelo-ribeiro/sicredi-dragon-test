22525581d3542b0ffb8f14ac1f43ba1c144f7e88
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2022-02-22 00:58:03 -0300

    Adicionado basename

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { GlobalStyle } from "./styles/global";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AuthProvider>
         <GlobalStyle />
         <App />
